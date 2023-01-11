b21a3e7fb4ccdf583584456c09cc48227ac6b92e
MonkeScripts <104839312+MonkeScripts@users.noreply.github.com>
2023-01-11 21:02:32 +0800

    Update 206. Reverse Linked List.cpp

--- 12 Days Crack Algo/206. Reverse Linked List.cpp
+++ 12 Days Crack Algo/206. Reverse Linked List.cpp
@@ -27,3 +27,27 @@ public:
         return prev;
     }
 };
+//Recursive solution
+/**
+ * Definition for singly-linked list->
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        //base case
+        if(!head || !(head->next)) return head;
+
+        //recursive case
+        ListNode* res = reverseList(head->next);    //generate the stack infrastructure via recursion
+        head->next->next = head; //code for each stack, starting fron the fisrt stack after base case
+        head->next = nullptr;
+        return res; // pop the stack
+    }
+};
