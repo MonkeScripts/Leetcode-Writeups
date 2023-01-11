68463dd079eede5135a623369baf6b02252cf103
MonkeScripts <104839312+MonkeScripts@users.noreply.github.com>
2023-01-11 17:45:02 +0800

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 12 Days Crack Algo/3. Longest Substring Without Repeating Characters.cpp
+++ 12 Days Crack Algo/3. Longest Substring Without Repeating Characters.cpp
@@ -11,14 +11,14 @@ public:
         while(right < s.size()){
             //increase the freq of the char in the array
             count[s[right]]++;
-          //when freq > 1 means that the same char has been visited before, we therefore have to rest the count array
-          //To rest, we use a while loop with the left pointer catching up to the right pointer
+          //when freq > 1 means that the same char has been visited before, we therefore have to reset the count array
+          //To reset, we use a while loop with the left pointer catching up to the right pointer
             while(count[s[right]] > 1){
                 count[s[left]] --;
                 left ++;
             }
             right++;
-            // find max length based o left and right pointers
+            // find max length based on the difference of left and right pointers
             max_length = max(right - left,max_length);  
         }
         return max_length;      
