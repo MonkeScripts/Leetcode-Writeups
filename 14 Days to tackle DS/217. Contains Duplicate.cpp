dad83033a97947efafe65861f769dda61c31c13a
MonkeScripts <104839312+MonkeScripts@users.noreply.github.com>
2023-03-06 15:27:22 +0800

    Update 217. Contains Duplicate.cpp

--- 14 Days to tackle DS/217. Contains Duplicate.cpp
+++ 14 Days to tackle DS/217. Contains Duplicate.cpp
@@ -4,13 +4,13 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         // Create a set...
-        unordered_set<int> ;hash
+        unordered_set<int> hash;
         // Traverse all the elements through the loop...
         for(int i = 0; i < nums.size(); ++i) {
             // Searches set. if present, it contains duplicate...
-            if(hset.count(nums[i])) return true;
+            if(hash.count(nums[i])) return true;
             // insert nums[i] in set...
-            hset.insert(nums[i]);
+            hash.insert(nums[i]);
         }
         return false;
     }
