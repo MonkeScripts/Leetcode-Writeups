1f42fcb5821458c0bb9c1db0105dc3da4fde173f
MonkeScripts <104839312+MonkeScripts@users.noreply.github.com>
2023-05-04 23:22:50 +0800

    Update SubSetSumRepeatingChar.cpp

--- DP Programming Algorithms Mastercourse 2022/2D DP:Subset Sum/SubSetSumRepeatingChar.cpp
+++ DP Programming Algorithms Mastercourse 2022/2D DP:Subset Sum/SubSetSumRepeatingChar.cpp
@@ -7,6 +7,7 @@ bool repeatingchar(int numbers[],int index,int target){
     if(memo[index][target] != -1) return memo[index][target];
     bool ans = false;
     //we can keep subtracting and check
+    //note that everytime we subtract the index we do not -1, this is so that we can repeatedly subtract the same array values
     if(target >= numbers[index]){
         ans |= repeatingchar(numbers,index,target - numbers[index]);
     }
